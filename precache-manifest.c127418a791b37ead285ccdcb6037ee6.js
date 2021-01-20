7795a1373421a776e11707701a07c9ac4e89c34c
ArmorDRX97 <armor.d.r.x@gmail.com>
2021-01-21 01:54:55 +0600

    init

---
+++ precache-manifest.c127418a791b37ead285ccdcb6037ee6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "798f810ec211acdb4b78",
+    "url": "/bootstrapvue-example/css/app.d0826a1d.css"
+  },
+  {
+    "revision": "87bb0dc379dd2a1d8012",
+    "url": "/bootstrapvue-example/css/chunk-vendors.01c183df.css"
+  },
+  {
+    "revision": "cb3d561b44d05a1854c743659f0e7c5c",
+    "url": "/bootstrapvue-example/index.html"
+  },
+  {
+    "revision": "9d35d6fca7d7c30f6c5b",
+    "url": "/bootstrapvue-example/js/about.f6092782.js"
+  },
+  {
+    "revision": "798f810ec211acdb4b78",
+    "url": "/bootstrapvue-example/js/app.aaa0398b.js"
+  },
+  {
+    "revision": "87bb0dc379dd2a1d8012",
+    "url": "/bootstrapvue-example/js/chunk-vendors.0fb3d0c2.js"
+  },
+  {
+    "revision": "55e93d53c603f46afc68c59b0416712c",
+    "url": "/bootstrapvue-example/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bootstrapvue-example/robots.txt"
+  }
+]);
